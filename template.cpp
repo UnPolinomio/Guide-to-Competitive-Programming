a3fcdc1c72303dff82ce12b7222b7035fe5786e7
Hector Franco T <tor6.franco6@hotmail.com>
2022-07-25 18:36:55 -0500

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fore(x, a, b) for (lli x = a, limit = b; x < limit; ++x)
+using namespace std;
+using lli = long long int;
+using ii = pair<lli, lli>;
+using vi = vector<lli>;
+
+int main() {
+  ;
+  return 0;
+}
