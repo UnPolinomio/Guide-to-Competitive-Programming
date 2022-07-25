407e722c275da32f5baf07fbb6888fe8fe5a8479
Hector Franco T <tor6.franco6@hotmail.com>
2022-07-25 15:24:11 -0500

    Add LIS O(nlogn)

--- 11_math/01_number_theory/01_lis.cpp
+++ 11_math/01_number_theory/01_lis.cpp
@@ -3,40 +3,24 @@
 using namespace std;
 using vi = vector<int>;
 
-vi lis(vi& v) {
-  if (v.size() == 0) return {};
-
-  int index{0};
-  vi maxs(v.size(), 1);
-  vi prev(v.size(), -1);
+int lis(vi& v) {
+  int ans{0};
+  vi aux(v.size() + 1, INT_MAX);
+  aux[0] = INT_MIN;
 
   fore(i, 0, v.size()) {
-    fore(j, 0, i) {
-      if (v[j] >= v[i] || maxs[j] < maxs[i]) continue;
-      maxs[i] = maxs[j] + 1;
-      prev[i] = j;
-    }
-    if (maxs[i] <= maxs[index]) continue;
-    index = i;
+    auto it{lower_bound(aux.begin(), aux.begin() + ans + 1, v[i])};
+    int new_length = it - aux.begin();
+    if (aux[new_length] == INT_MAX) ++ans;
+    aux[new_length] = min(aux[new_length], v[i]);
   }
-
-  vi ans{};
-  while (index != -1) {
-    ans.push_back(v[index]);
-    index = prev[index];
-  }
-  reverse(ans.begin(), ans.end());
 
   return ans;
 }
 
 int main() {
   vi nums{6, 2, 5, 1, 7, 4, 8, 3};
-  vi ans{lis(nums)};
-  for (int num : ans) {
-    cout << num << ' ';
-  }
-  cout << '\n';
+  cout << lis(nums) << '\n';
 
   return 0;
 }
