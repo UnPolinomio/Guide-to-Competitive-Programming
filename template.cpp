87e48e540750855dc348cf87ce1829e8dbb2df29
Hector Franco T <42476070+UnPolinomio@users.noreply.github.com>
2022-08-03 09:42:16 -0500

    Update template

--- template.cpp
+++ template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
-#define fore(x, a, b) for (lli x = a, limit = b; x < limit; ++x)
+#define fore(x, a, b) for (lli x = a, ___limit___ = b; x < ___limit___; ++x)
 #define all(x) x.begin(), x.end()
+#define deb(x) cout << #x << (x) << '\n';
 using namespace std;
 using lli = long long int;
 using ii = pair<lli, lli>;
