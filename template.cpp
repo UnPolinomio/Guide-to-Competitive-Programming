1a49930fe4d7d676457eb23d6fcf8ce9744c8d23
Hector Franco T <tor6.franco6@hotmail.com>
2022-08-02 20:35:03 -0500

    Update template

--- template.cpp
+++ template.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 #define fore(x, a, b) for (lli x = a, limit = b; x < limit; ++x)
+#define all(x) x.begin(), x.end()
 using namespace std;
 using lli = long long int;
 using ii = pair<lli, lli>;
 using vi = vector<lli>;
+using vb = vector<bool>;
 
 int main() {
   ;
