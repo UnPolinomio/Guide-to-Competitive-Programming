59947e192d4e96c2eb0cb93c398e377228ca9f80
Hector Franco T <42476070+UnPolinomio@users.noreply.github.com>
2022-08-07 15:53:42 -0500

    Fix template deb(x)

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define fore(x, a, b) for (lli x = a, ___limit___ = b; x < ___limit___; ++x)
 #define all(x) x.begin(), x.end()
-#define deb(x) cout << #x << (x) << '\n';
+#define deb(x) cout << #x << ": " << (x) << '\n';
 using namespace std;
 using lli = long long int;
 using ii = pair<lli, lli>;
