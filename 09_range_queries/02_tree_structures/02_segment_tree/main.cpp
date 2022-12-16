54062533c3635a7f87e172891e06cc02e0d5322e
Hector Franco T <42476070+UnPolinomio@users.noreply.github.com>
2022-12-15 18:58:30 -0600

    Update segment tree for any function

--- 09_range_queries/02_tree_structures/02_segment_tree/main.cpp
+++ 09_range_queries/02_tree_structures/02_segment_tree/main.cpp
@@ -8,30 +8,37 @@ using ii = pair<lli, lli>;
 using vi = vector<lli>;
 using vb = vector<bool>;
 
+using Combiner = function<lli(lli, lli)>;
+
 struct SegmentTree {
   vi t{};
   lli n{};
+  Combiner f{};
+  lli neutral{};
 
-  SegmentTree(initializer_list<lli> l) {
+  SegmentTree(
+      initializer_list<lli> l, Combiner f = [](lli a, lli b) { return a + b; },
+      lli neutral = 0)
+      : f(f), neutral(neutral) {
     n = l.size();
     t.resize(2 * n);
     copy(all(l), t.begin() + n);
-    for (lli i{n - 1}; i > 0; --i) t[i] = t[i << 1] + t[i << 1 | 1];
+    for (lli i{n - 1}; i > 0; --i) t[i] = f(t[i << 1], t[i << 1 | 1]);
   }
 
   void update(lli i, lli v) {
     i += n;
     t[i] = v;
-    for (i >>= 1; i > 0; i >>= 1) t[i] = t[i << 1] + t[i << 1 | 1];
+    for (i >>= 1; i > 0; i >>= 1) t[i] = f(t[i << 1], t[i << 1 | 1]);
   }
 
   lli query(lli l, lli r) {
     l += n, r += n;
-    lli ans{0};
+    lli ans{neutral};
 
     while (l <= r) {
-      if (l & 1) ans += t[l++];
-      if (!(r & 1)) ans += t[r--];
+      if (l & 1) ans = f(ans, t[l++]);
+      if (!(r & 1)) ans = f(ans, t[r--]);
       l >>= 1, r >>= 1;
     }
 
@@ -46,7 +53,18 @@ int main() {
   cout << st.query(0, 3) << '\n';
   cout << st.query(1, 2) << '\n';
   cout << st.query(0, 0) << '\n';
-  cout << st.query(3, 3) << '\n';
   cout << st.query(2, 2) << '\n';
+
+  cout << "---------------------\n";
+
+  SegmentTree st2{{500, 2, 10, 4},
+                  [](lli a, lli b) { return min(a, b); },
+                  numeric_limits<lli>::max()};
+  cout << st2.query(0, 3) << '\n';
+  cout << st2.query(1, 2) << '\n';
+  cout << st2.query(0, 0) << '\n';
+  cout << st2.query(2, 2) << '\n';
+  cout << st2.query(2, 3) << '\n';
+
   return 0;
 }
